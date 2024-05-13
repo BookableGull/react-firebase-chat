5191ef5b7182318634d1060e82afda78d7e3e1c8
BookableGull <ivotomasovich@gmail.com>
2024-05-12 22:52:18 -0500

    added react functions; about 2/3rds done

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -6,12 +6,12 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCs7hp1wOJpUq5qOKhz2PFqexrn2-mpyxM",
-  authDomain: "reactchat-8a047.firebaseapp.com",
-  projectId: "reactchat-8a047",
-  storageBucket: "reactchat-8a047.appspot.com",
-  messagingSenderId: "782691037530",
-  appId: "1:782691037530:web:2bd0df3afa1084f2800363"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: "reactchat-7f1a6.firebaseapp.com",
+  projectId: "reactchat-7f1a6",
+  storageBucket: "reactchat-7f1a6.appspot.com",
+  messagingSenderId: "245218741740",
+  appId: "1:245218741740:web:763c2f44124d7b2348c4c9"
 };
 
 // Initialize Firebase
